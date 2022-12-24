7ad54222fa148c0f060953547026bd1b0ca2f1c5
Vanja Jankovic <vj20190010@student.fon.bg.ac.rs>
2022-12-24 12:40:55 +0100

    kreirane migracije i modeli sa vezama

---
+++ blog-library/app/Models/Book.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    use HasFactory;
+
+    public function author(){
+        return $this->belongsTo(Author::class);
+    }
+
+    public function genre(){
+        return $this->belongsTo(Genre::class);
+    }
+
+    public function publisher(){
+        return $this->belongsTo(Publisher::class);
+    }
+}
